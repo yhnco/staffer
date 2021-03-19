a1aad09c0f066589e97ef7899c7d8884576b8bd6
yhnco <65667037+yhnco@users.noreply.github.com>
2021-03-19 09:27:09 -0400

    first commit

---
+++ resources/views/connexion.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Staffer - Connexion</title>
+</head>
+<body>
+    <p>Bonjour et bienvenue sur Staffer!</p>
+    <p>Veuillez vous connectez ou vous enregistrer.</p>
+</body>
+</html>
